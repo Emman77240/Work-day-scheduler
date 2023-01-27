6a65e85e8dd281fb4ea8dc96c51b9adaaddf45f3
Emman77240 <asiegbuemmanuelekee@gmail.com>
2023-01-27 16:26:39 +0000

    Added times to respective time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Initialize global variables
-let timeOfDay = "am"; 
 let startTime = 9;
+let newTime = 1;
 let timeCount = 1;
 let myClass = "row";
 
@@ -9,7 +9,7 @@ let myClass = "row";
 $(document).ready(function () {
 
     // Create time block
-    for(let i = 0; i < 12; i++) {
+    for(let i = 0; i < 9; i++) {
         // Create time block
         let timeBlock = "<div class=" + myClass + "><div class=" + "text" + timeCount + "></div><textarea></textarea><button></button></div>";
         $(".container").append(timeBlock);
@@ -17,4 +17,20 @@ $(document).ready(function () {
         $("button").addClass("saveBtn");
         timeCount++;
     }
+
+    // Add time to time block
+    for(let i = 1; i < 10; i++) {
+        if(startTime < 12) {
+            $(".text" + i).text(startTime + "AM");
+            startTime++;
+        } else if(startTime === 12) {
+            $(".text" + i).text(startTime + "PM");
+            startTime++;
+        } else {
+            $(".text" + i).text(newTime + "PM");
+            newTime++;
+        }
+    }
+
+
   });
